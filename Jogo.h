500bf864f281c3622e6ae828a2068f3a238f8e5d
Fbnascimento <53975267+Fbnascimento@users.noreply.github.com>
2019-11-14 00:49:55 -0300

    Delete Jogo.h

--- Jogo.h
+++
@@ -1,7 +0,0 @@
-#ifndef JOGO_H_INCLUDED
-#define JOGO_H_INCLUDED
-
-void jogar(SDL_Surface* ecran);
-void movimentojogador1(int mapa[13][15], SDL_Rect *pos, int direcao);
-
-#endif // JOGO_H_INCLUDED
